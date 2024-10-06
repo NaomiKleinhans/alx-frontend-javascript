5d45d15bef6648f709173ccc96e26d7f512ecb53
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-10-06 10:49:20 +0200

    Update 9-try.js

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,12 +1,12 @@
 export default function guardrail(mathFunction) {
-	const queue = [];
+  const queue = [];
 
-	try {
-		queue.push(mathFunction());
-	} catch (err) {
-		queue.push(err.toString());
-	} finally {
-		queue.push('Guardrail was processed');
+  try {
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(err.toString());
+  } finally {
+    queue.push('Guardrail was processed');
 	}
-	return queue;
+  return queue;
 }
