3b92a2f0f3a25f7b8f4fa31bd851442a28921f01
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-10-06 08:30:51 +0200

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,11 +2,8 @@ export default function taskBlock(trueOrFalse) {
 	const task = false;
 	const task2 = true;
 
-  if (trueOrFalse) {
-		const newTask = true;
-		const newTask2 = false;
-		return [newTask, newTask2];
-  }
-
+	if (trueOrFalse) {
+		return [task, task2];
+	}
 	return [task, task2];
 }
