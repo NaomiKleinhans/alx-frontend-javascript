fe16bcfff91d4bfd3f0642a33b0a73d0ede1901a
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-10-06 10:41:19 +0200

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((value) => {
       console.log(
-        `${value[0].body} ${value[1].firstName} ${value[1].lastName}`
+        `${value[0].body} ${value[1].firstName} ${value[1].lastName}`,
       );
     })
 
