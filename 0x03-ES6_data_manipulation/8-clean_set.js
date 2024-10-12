5f5d7465d14d6ab425bbe5314623fc4f62ecded2
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-10-12 17:07:34 +0200

    Update 8-clean_set.js

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+// Function that returns a string of set values starting with a specific string
+const cleanSet = (set, startString) => {
+  const values = [...set]
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
+    .join('-');
+
+  return values;
+};
+
+// Export the function as default
+export default cleanSet;
