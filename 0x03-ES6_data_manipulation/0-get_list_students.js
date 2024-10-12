2b7d3eab726a1e00f04c71d965807d722b8f5515
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-10-12 15:46:28 +0200

    task 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,10 @@
+// Arrow function that returns an array of student objects
+const getListStudents = () => [
+	// Returning an array of objects, each representing a student with id, firstName, and location properties
+	{ id: 1, firstName: 'Guillaume', location: 'San Francisco' }, // Student 1
+	{ id: 2, firstName: 'James', location: 'Columbia' }, // Student 2
+	{ id: 5, firstName: 'Serena', location: 'San Francisco' } // Student 3
+];
+
+// Exporting the arrow function as default
+export default getListStudents;
