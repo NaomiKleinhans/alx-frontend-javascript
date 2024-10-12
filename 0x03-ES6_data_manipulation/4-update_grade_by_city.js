67817a1d45b1f048564eb24460a5fac437922045
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-10-12 16:38:31 +0200

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,17 +1,17 @@
 // Arrow function that updates student grades by city
 const updateStudentGradeByCity = (students, city, newGrades) => {
   // Filter students based on the city
-  return students
+  students
     .filter((student) => student.location === city)
     .map((student) => {
       // Find the grade for the current student, or default to 'N/A'
       const gradeObj = newGrades.find(
-        (grade) => grade.studentId === student.id
+        (grade) => grade.studentId === student.id,
       );
       // Return the updated student object with the grade
       return {
         ...student,
-        grade: gradeObj ? gradeObj.grade : 'N/A'
+        grade: gradeObj ? gradeObj.grade : 'N/A',
       };
     });
 };
