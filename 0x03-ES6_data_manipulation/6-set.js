6cab95d77c1e098108d6ff64627ce2a220517623
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-10-12 17:04:15 +0200

    Update 6-set.js

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,6 +1,5 @@
 // Function that creates a Set from an array
-const setFromArray = (array) =>
-  new Set(array);
+const setFromArray = (array) => new Set(array);
 
 // Export the function as default
 export default setFromArray;
