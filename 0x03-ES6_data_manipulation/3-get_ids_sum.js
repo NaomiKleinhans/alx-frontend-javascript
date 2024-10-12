048c74a8ff1ce443d160885c59a1cca95b915316
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-10-12 16:32:10 +0200

    Update 3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,5 @@
 // Arrow function that returns the sum of all student ids
 const getStudentIdsSum = (students) =>
-  // Use the reduce method to sum up the ids of the students
   students.reduce((sum, student) => sum + student.id, 0);
 
 // Export the arrow function as default
