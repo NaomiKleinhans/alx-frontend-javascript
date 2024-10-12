9ec53f307336ec7b1d02a8799e4bbf1be19fd4c8
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-10-12 17:18:09 +0200

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,13 @@
 // Function that returns a string of set values starting with a specific string
 const cleanSet = (set, startString) => {
+  // Check if startString is not a string or is undefined
   if (typeof startString !== 'string') return '';
+
+  // Convert set to an array, filter, map, and join values
   const values = [...set]
-    .filter((value) => value.startsWith(startString))
+    .filter(
+      (value) => typeof value === 'string' && value.startsWith(startString)
+    )
     .map((value) => value.slice(startString.length))
     .join('-');
 
